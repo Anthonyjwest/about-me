ee63c1f9794d2c594bd3c3a0258d38cb3b9006e6
Anthony Westerfield <Anthonywestj98@gmail.com>
2023-12-12 22:49:32 -0700

    java finished questions

--- js/app.js
+++ js/app.js
@@ -4,12 +4,15 @@ let username = prompt('What is your name?');
 console.log(username);
 alert(`Hey ${username} welcome to my site! Lets play a guessing game, answer with yes/no OR y/n!`);
 
+let rightanswer = 0;
+
 let questionOne = prompt('Was I born in New York?').toLowerCase();
 
 //console.log(questionOne);
 
 if (questionOne === 'y' || questionOne === 'yes'){
   alert('You are correct!');
+  rightanswer += 1;
 } else if(questionOne === 'n' || questionOne === 'no'){
   alert('That is incorrect, I was born in New York');
 }
@@ -20,6 +23,7 @@ let questionTwo = prompt('I have lived in New York for my whole life?').toLowerC
 
 if (questionTwo === 'n' || questionTwo === 'no'){
   alert('You are correct!');
+  rightanswer += 1;
 } else if(questionTwo === 'y' || questionTwo === 'yes'){
   alert('That is incorrect, I have lived in six differnt states in my life');
 }
@@ -30,6 +34,7 @@ let questionThree = prompt('I was varsity in track my freshmen year of Highschoo
 
 if (questionThree === 'y' || questionThree === 'yes'){
   alert('You are correct!');
+  rightanswer += 1;
 } else if(questionThree === 'n' || questionThree === 'no'){
   alert('That is incorrect, I was varsity for long jump and triple jump!');
 }
@@ -40,6 +45,7 @@ let questionFour = prompt('I am the only child in my family?').toLowerCase();
 
 if (questionFour === 'n' || questionFour === 'no'){
   alert('You are correct!');
+  rightanswer += 1;
 } else if(questionFour === 'y' || questionFour === 'yes'){
   alert('That is incorrect, I have one sister!');
 }
@@ -50,9 +56,44 @@ let questionFive = prompt('My favorite fast food chain is Burger King?').toLower
 
 if (questionFive === 'y' || questionFive === 'yes'){
   alert('You are correct!');
+  rightanswer += 1;
 } else if(questionFive === 'n' || questionFive === 'no'){
   alert('That is incorrect, I love Burger King!');
 }
 
-alert(`Thanks ${username}, I hope you learned a little bit more about me today!`);
+for (let g = 0; g < 4; g++){
+  let myNum = '87';
+  let questionSix = prompt('Can you guess my favorite football players jersey number? Hint: He played for the colts and was a wide reciever! You have four guesses! Good luck!');
+  if (questionSix === myNum){
+    alert('That is correct! Reggie Wayne was my favorite player!');
+    rightanswer += 1;
+    g +=4;
+  } else if (questionSix < myNum) {
+    alert('Too Low! Try again');
+  } else if (questionSix > myNum) {
+    alert('Too High! Try again');
+  }
 
+  if (g === 3) {
+    alert('The correct answer was 87. Reggie Waynes Number!');
+  }
+}
+
+let myAnswers = ['kitkat', 'milkyway', 'reeses', 'twix', 'snickers'];
+
+for (let g = 0; g < 6; g++){
+  let questionSeven = prompt('Name one of my favorite candy bars! You have six attempts!').toLowerCase();
+
+  for (let i = 0; i < myAnswers.length; i++){
+    if (questionSeven === myAnswers[i]){
+      alert('Correct! I love those!');
+      rightanswer += 1;
+      g +=6;
+      break;
+    }
+  }
+}
+
+alert(`Here are my favorite Candy bars! ${myAnswers}`);
+
+alert(`Thanks ${username}, I hope you learned a little bit more about me today! You scored ${rightanswer} out of 7!`);
