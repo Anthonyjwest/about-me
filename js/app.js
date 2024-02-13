d036e44a786c886706906c8753f2e6c710f6a5e0
Anthony Westerfield <Anthonywestj98@gmail.com>
2024-02-12 18:48:53 -0700

    added functions

--- js/app.js
+++ js/app.js
@@ -6,11 +6,12 @@ alert(`Hey ${username} welcome to my site! Lets play a guessing game, answer wit
 
 let rightanswer = 0;
 
-function questionOne() {
+function onequestion(){
+
   let questionOne = prompt('Was I born in New York?').toLowerCase();
-  
+
   console.log(questionOne);
-  
+
   if (questionOne === 'y' || questionOne === 'yes'){
     alert('You are correct!');
     rightanswer += 1;
@@ -18,83 +19,115 @@ function questionOne() {
     alert('That is incorrect, I was born in New York');
   }
 }
-let questionTwo = prompt('I have lived in New York for my whole life?').toLowerCase();
 
-console.log(questionTwo);
+onequestion();
 
-if (questionTwo === 'n' || questionTwo === 'no'){
-  alert('You are correct!');
-  rightanswer += 1;
-} else if(questionTwo === 'y' || questionTwo === 'yes'){
-  alert('That is incorrect, I have lived in six differnt states in my life');
-}
+function twoquestion(){
 
-let questionThree = prompt('I was varsity in track my freshmen year of Highschool?').toLowerCase();
+  let questionTwo = prompt('I have lived in New York for my whole life?').toLowerCase();
 
-console.log(questionThree);
+  console.log(questionTwo);
 
-if (questionThree === 'y' || questionThree === 'yes'){
-  alert('You are correct!');
-  rightanswer += 1;
-} else if(questionThree === 'n' || questionThree === 'no'){
-  alert('That is incorrect, I was varsity for long jump and triple jump!');
-}
-
-let questionFour = prompt('I am the only child in my family?').toLowerCase();
-
-console.log(questionFour);
-
-if (questionFour === 'n' || questionFour === 'no'){
-  alert('You are correct!');
-  rightanswer += 1;
-} else if(questionFour === 'y' || questionFour === 'yes'){
-  alert('That is incorrect, I have one sister!');
-}
-
-let questionFive = prompt('My favorite fast food chain is Burger King?').toLowerCase();
-
-console.log(questionFive);
-
-if (questionFive === 'y' || questionFive === 'yes'){
-  alert('You are correct!');
-  rightanswer += 1;
-} else if(questionFive === 'n' || questionFive === 'no'){
-  alert('That is incorrect, I love Burger King!');
-}
-
-for (let g = 0; g < 4; g++){
-  let myNum = '87';
-  let questionSix = prompt('Can you guess my favorite football players jersey number? Hint: He played for the colts and was a wide reciever! You have four guesses! Good luck!');
-  if (questionSix === myNum){
-    alert('That is correct! Reggie Wayne was my favorite player!');
+  if (questionTwo === 'n' || questionTwo === 'no'){
+    alert('You are correct!');
     rightanswer += 1;
-    g +=4;
-  } else if (questionSix < myNum) {
-    alert('Too Low! Try again');
-  } else if (questionSix > myNum) {
-    alert('Too High! Try again');
-  }
-
-  if (g === 3) {
-    alert('The correct answer was 87. Reggie Waynes Number!');
+  } else if(questionTwo === 'y' || questionTwo === 'yes'){
+    alert('That is incorrect, I have lived in six differnt states in my life');
   }
 }
 
-let myAnswers = ['kitkat', 'milkyway', 'reeses', 'twix', 'snickers'];
+twoquestion();
 
-for (let g = 0; g < 6; g++){
-  let questionSeven = prompt('Name one of my favorite candy bars! You have six attempts!').toLowerCase();
+function threequestion(){
 
-  for (let i = 0; i < myAnswers.length; i++){
-    if (questionSeven === myAnswers[i]){
-      alert('Correct! I love those!');
+  let questionThree = prompt('I was varsity in track my freshmen year of Highschool?').toLowerCase();
+
+  console.log(questionThree);
+
+  if (questionThree === 'y' || questionThree === 'yes'){
+    alert('You are correct!');
+    rightanswer += 1;
+  } else if(questionThree === 'n' || questionThree === 'no'){
+    alert('That is incorrect, I was varsity for long jump and triple jump!');
+  }
+}
+
+threequestion();
+
+function fourquestion(){
+
+  let questionFour = prompt('I am the only child in my family?').toLowerCase();
+
+  console.log(questionFour);
+
+  if (questionFour === 'n' || questionFour === 'no'){
+    alert('You are correct!');
+    rightanswer += 1;
+  } else if(questionFour === 'y' || questionFour === 'yes'){
+    alert('That is incorrect, I have one sister!');
+  }
+}
+
+fourquestion();
+
+function fivequestion(){
+
+  let questionFive = prompt('My favorite fast food chain is Burger King?').toLowerCase();
+
+  console.log(questionFive);
+
+  if (questionFive === 'y' || questionFive === 'yes'){
+    alert('You are correct!');
+    rightanswer += 1;
+  } else if(questionFive === 'n' || questionFive === 'no'){
+    alert('That is incorrect, I love Burger King!');
+  }
+}
+
+fivequestion();
+
+function sixquestion(){
+
+  for (let g = 0; g < 4; g++){
+    let myNum = '87';
+    let questionSix = prompt('Can you guess my favorite football players jersey number? Hint: He played for the colts and was a wide reciever! You have four guesses! Good luck!');
+    if (questionSix === myNum){
+      alert('That is correct! Reggie Wayne was my favorite player!');
       rightanswer += 1;
-      g +=6;
-      break;
+      g +=4;
+    } else if (questionSix < myNum) {
+      alert('Too Low! Try again');
+    } else if (questionSix > myNum) {
+      alert('Too High! Try again');
+    }
+
+    if (g === 3) {
+      alert('The correct answer was 87. Reggie Waynes Number!');
     }
   }
 }
 
-alert(`Here are my favorite Candy bars! ${myAnswers}`);
+sixquestion();
+
+function sevenquestion(){
+
+  let myAnswers = ['kitkat', 'milkyway', 'reeses', 'twix', 'snickers'];
+
+  for (let g = 0; g < 6; g++){
+    let questionSeven = prompt('Name one of my favorite candy bars! You have six attempts!').toLowerCase();
+
+    for (let i = 0; i < myAnswers.length; i++){
+      if (questionSeven === myAnswers[i]){
+        alert('Correct! I love those!');
+        rightanswer += 1;
+        g +=6;
+        break;
+      }
+    }
+  }
+  alert(`Here are my favorite Candy bars! ${myAnswers}`);
+}
+
+sevenquestion();
 
 alert(`Thanks ${username}, I hope you learned a little bit more about me today! You scored ${rightanswer} out of 7!`);
